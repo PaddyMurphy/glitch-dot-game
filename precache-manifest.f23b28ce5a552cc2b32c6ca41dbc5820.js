0c470bafbdb40a44c941bc1e445f4fd34aabec39
plewis <plewis@indeed.com>
2019-02-04 10:05:34 -0600

    Updates

---
+++ precache-manifest.f23b28ce5a552cc2b32c6ca41dbc5820.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d80d2bccd2a12722e1c",
+    "url": "/glitch-dot-game/static/js/runtime~main.9d80d2bc.js"
+  },
+  {
+    "revision": "16715210766b26f92f13",
+    "url": "/glitch-dot-game/static/js/main.16715210.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/glitch-dot-game/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "16715210766b26f92f13",
+    "url": "/glitch-dot-game/static/css/main.f063a8b6.chunk.css"
+  },
+  {
+    "revision": "623d603ff83022f436dc6826f2a947a8",
+    "url": "/glitch-dot-game/index.html"
+  }
+];
